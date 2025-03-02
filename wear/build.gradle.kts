308635f3593413bf4e780f8101c46e0050ca7594
Trimpsuz <me@trimpsuz.dev>
2025-03-02 23:54:07 +0200

    bump version

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = appPackageName
         minSdk = 30
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
 
     }
 
