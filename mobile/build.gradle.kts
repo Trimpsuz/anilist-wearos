cee64634148c31c23f137da108cd60c10b765513
Trimpsuz <me@trimpsuz.dev>
2025-07-31 02:19:34 +0300

    bump version

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = appPackageName
         minSdk = 30
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
